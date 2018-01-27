6c4b5aa12cbc1e9d9715425d45fa30fb0e43c0c0
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-27 18:39:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,17 +3,17 @@ function getIt(){
   return;
 }
 
-function frameIt(){
+function submitIt(){
   $('img').on('load', function(){
     this.className = 'tasty'
   })
 }
 function pressIt(){
-  
+  alert('Your form is going to be submitted now.')
 }
 $(document).ready(function(){
 
 $('p').on('click', getIt);
 frameIt()
-
+$('form').on('submit', submitIt)
 });
